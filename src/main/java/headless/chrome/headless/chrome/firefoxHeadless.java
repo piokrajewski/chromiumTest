8572bd66be8051bed969830bc41d6c434ad03b52
pkrajewski <piotrkrajewski55@gmail.com>
2018-05-22 18:04:48 +0200

    final

--- src/main/java/headless/chrome/headless/chrome/firefoxHeadless.java
+++ src/main/java/headless/chrome/headless/chrome/firefoxHeadless.java
@@ -2,6 +2,9 @@ package headless.chrome.headless.chrome;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
@@ -11,20 +14,28 @@ import junit.framework.Assert;
 
 public class firefoxHeadless  {
 
+//	@Test
+//	public void test32()  {
+//		
+//	    FirefoxBinary firefoxBinary = new FirefoxBinary();
+//	    firefoxBinary.addCommandLineOptions("--headless");
+//	    System.setProperty("webdriver.gecko.driver", "/root/geckodriver");
+//	    FirefoxOptions firefoxOptions = new FirefoxOptions();
+//	    firefoxOptions.setBinary(firefoxBinary);
+//	    FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+//		
+//		driver.get("http://www.google.pl");
+//		driver.findElement(By.name("q")).sendKeys("Selenium",Keys.ENTER);
+//		
+//		driver.findElement(By.cssSelector("div.rc h3:nth-child(1)")).click();
+//		Assert.assertEquals("Pierwsze kroki z Selenium – Selenium IDE - Kainos Polska", driver.getTitle());
+//	}
 	@Test
-	public void test32()  {
+	public void test33() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
 		
-	    FirefoxBinary firefoxBinary = new FirefoxBinary();
-	    firefoxBinary.addCommandLineOptions("--headless");
-	    System.setProperty("webdriver.gecko.driver", "/root/geckodriver");
-	    FirefoxOptions firefoxOptions = new FirefoxOptions();
-	    firefoxOptions.setBinary(firefoxBinary);
-	    FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
-		
+		WebDriver driver = new ChromeDriver(options);
 		driver.get("http://www.google.pl");
-		driver.findElement(By.name("q")).sendKeys("Selenium",Keys.ENTER);
-		
-		driver.findElement(By.cssSelector("div.rc h3:nth-child(1)")).click();
-		Assert.assertEquals("Pierwsze kroki z Selenium – Selenium IDE - Kainos Polska", driver.getTitle());
 	}
 }
